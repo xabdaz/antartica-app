c8ad398ead36f87388b0cae9037ce3a7e8db4bd6
Asep Abdaz <asepabdaz@gmail.com>
2021-11-20 22:27:11 +0700

    initial commit

---
+++ Learn/variable.py
@@ -0,0 +1,13 @@
+a = 10
+x = 5
+panjang = 1000
+
+print("Nilai a = ", a)
+
+juta10 = 100_000_000
+print(juta10)
+juta10 = 10
+print(juta10)
+# assignment indirect
+b = juta10
+print(b)
---
+++ Main.py
@@ -0,0 +1,6 @@
+import time
+start_time = time.time()
+
+print("Hallo World")
+
+print(time.time() - start_time, "detik")
